f2b11200548bca98c3b18e58049afd3187e38df8
diegoglobe <diego@DESKTOP-S9HORA7.localdomain>
2025-11-07 19:22:43 +0100

    nuova versione di nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,34 +1,34 @@
 {
   description = "Ambiente di test per l'applicativo Spring/Angular";
 
-  # 1. INPUTS
-  # Definiamo le nostre due fonti di pacchetti
   inputs = {
-    # Fonte principale e aggiornata
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
-    # Fonte "vecchia" per MariaDB 10.4
     nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-23.05";
   };
 
-  # 2. OUTPUTS
-  # Assicurati che "nixpkgs-old" sia qui negli argomenti
   outputs = { self, nixpkgs, nixpkgs-old, ... }:
     let
-      # Definiamo il sistema
       system = "x86_64-linux";
       
       # Pacchetti dal canale NUOVO (unstable)
       pkgs = nixpkgs.legacyPackages.${system};
       
-      # Definiamo i pacchetti dal canale VECCHIO
-      # Assicurati che "pkgs-old" sia definito qui
-      pkgs-old = nixpkgs-old.legacyPackages.${system};
+      # MODIFICA QUI:
+      # Pacchetti dal canale VECCHIO
+      # Importiamo 'nixpkgs-old' come una funzione per passargli
+      # la nostra configurazione di override per i pacchetti insicuri.
+      pkgs-old = import nixpkgs-old {
+        inherit system;
+        config = {
+          # Autorizziamo specificamente OpenSSL 1.1.1w
+          permittedInsecurePackages = [
+            "openssl-1.1.1w"
+          ];
+        };
+      };
 
     in
     {
-      # 3. La Shell
-      # Tutto ciò che è nel blocco 'let' è visibile qui
       devShells.${system}.default = pkgs.mkShell {
         
         buildInputs = [
@@ -39,7 +39,8 @@
           pkgs.git
 
           # Pacchetto da 'pkgs-old' (vecchio)
-          pkgs-old.mariadb_104 
+          # Ora questo è autorizzato a usare openssl 1.1.1w
+          pkgs-old.mariadb_10_4
         ];
       };
     };
